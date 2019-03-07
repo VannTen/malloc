17b7d53af23882643dd9277b724b9e3e829ac931
Max Gautier <mgautier@student.42.fr>
2019-03-07 16:28:56 +0100

    Init a new alloc zone

--- create_zone.c
+++ create_zone.c
@@ -12,35 +12,36 @@
 
 #include <sys/mman.h>
 
-static struct s_free_node	*get_first_node(void *start)
-{
-	char				*addr;
-	struct s_free_node	*node;
-
-	addr = start;
-	addr += sizeof(struct s_alloc_zone);
-	addr += fixed_offset();
-	addr = align_addr(addr);
-	node = addr - fixed_offset(); // TODO : how do I determine it ?
-	return (node);
-}
+/*
+** - allocate a mapping
+** - compute the first aligned address in the mapping that will allow to put a
+** s_alloc_zone + a s_free_node before it.
+** - write the first free node.
+**
+**
+** Assumptions : Address returned by mmap are aligned on the aligment used by
+** align ( will probably be 16), so the offset between the address returned by
+** mmap (the start of the mapping) and the first_client_address will always be
+** the same.
+*/
 
 struct s_alloc_zone	*create_zone(size_t	size)
 {
 	void				*zone_start;
 	struct s_alloc_zone	*alloc_zone;
-	struct s_free_node	*node;
+	struct s_free_node	*first_node;
+	void				*first_client_address;
 
 	zone_start = mmap(NULL, size,
 			PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE,
 			VM_MAKE_TAG(get_zone_type(size)), 0);
 	if (zone_start == MAP_FAILED)
 		return (NULL);
-	if (get_zone_type(size) != LARGE)
-	{
-		alloc_zone = zone_start;
-		alloc_zone->type = get_zone_type(size);
-		node = get_first_node(zone_start);
-	}
+	first_client_address = zone_start + sizeof *alloc_zone + sizeof *first_node;
+	align(&first_client_address);
+	first_node = first_client_address - sizeof *first_node;
+	first_node->next = first_node;
+	first_node->free = true;
+	alloc_zone->biggest_size = compute_size(first_node, /* type */ );
 	return (zone_start);
 }
