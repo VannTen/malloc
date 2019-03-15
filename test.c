c36c64e095989d3d12e4448f15575a7d759e40a2
Max Gautier <mgautier@student.42.fr>
2019-03-15 16:16:49 +0100

    test page exhaustion

--- test.c
+++ test.c
@@ -20,18 +20,22 @@ struct s_alloc_zone	*create_zone(size_t	size);
 int main( void )
 {
 	struct s_alloc_zone * zone;
+	void const			* ptr;
 
 	zone = create_zone(4096);
 	assert(assert_create_zone(zone));
 	//munmap(zone, 4096);
 
 	printf("%p\n", (void*)zone);
-	assert(nb_free_node(zone) == 1);
+	assert(nb_node(zone) == 1);
 	printf("%p\n", get_first_fit(zone, 50));
-	assert(nb_free_node(zone) == 2);
+	assert(nb_node(zone) == 2);
 	printf("%p\n", get_first_fit(zone, 13));
-	assert(nb_free_node(zone) == 3);
+	assert(nb_node(zone) == 3);
 	printf("%p\n", get_first_fit(zone, 13));
-	assert(nb_free_node(zone) == 4);
+	assert(nb_node(zone) == 4);
+	while ((ptr = get_first_fit(zone, 23)) != NULL)
+		printf("%p\n", ptr);
+	printf("%zu\n", nb_node(zone));
 	return (0);
 }
