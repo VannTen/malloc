83b5654bc8ab8beedb49a84731de2bbef096ff17
Max Gautier <mgautier@student.42.fr>
2019-04-18 14:36:05 +0200

    add check to see if tree is correctly ordered

--- rb_tree/rbtree_test_helpers.c
+++ rb_tree/rbtree_test_helpers.c
@@ -24,3 +24,23 @@ int rbtree_test_diff(void const *val_1, void const *crit)
 	return (((struct s_test_node const *)val_1)->value
 			- *(int*)crit);
 }
+
+static void	acc(void const *_node, void *prev_val)
+{
+	struct s_test_node const * const	node = _node;
+	int * const							val = prev_val;
+
+	val[1] = val[1] && node->value >= val[0];
+	val[0] = node->value;
+
+}
+
+int	tree_is_inorder(struct s_rbtree const *tree)
+{
+	int value[2];
+
+	value[0] = 0;
+	value[1] = (0 == 0);
+	rbtree_inorder_traversal(tree, acc, value);
+	return (value[1]);
+}
