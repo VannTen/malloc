7b32a01e9f10af53760e7402e764a754c86f81ae
Max Gautier <mgautier@student.42.fr>
2019-05-16 14:23:06 +0200

    update test

--- test_src/multi_malloc_free.c
+++ test_src/multi_malloc_free.c
@@ -11,15 +11,23 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int	main(void)
 {
 	size_t	index;
+	void	*address;
 
 	index = 0;
 	while (index < 100000)
 	{
-		free(malloc(10));
+		address = malloc(10);
+		if (address == NULL)
+		{
+			printf("Failed malloc at %zu attempt\n", index);
+			return (1);
+		}
+		free(address);
 		index++;
 	}
 	return (0);
