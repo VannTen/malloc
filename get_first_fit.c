09cd5816fbc34c866a50943eb3b913c6f1cfeae4
Max Gautier <mgautier@student.42.fr>
2019-05-17 15:03:47 +0200

    fix incorrect carving of new nodes

--- get_first_fit.c
+++ get_first_fit.c
@@ -32,7 +32,7 @@ static void	carve_node(struct s_free_node * node, size_t size_required)
 	assert(node_has_enough_space(node, size_required)
 			&& node_size(node)
 				>= size_required + sizeof *node + MIN_ALLOC_SPACE);
-	new_node = (struct s_free_node *)((char*)(node + 1)
+	new_node = (struct s_free_node *)((char*)(node)
 			+ round_up_to_multiple(size_required, ft_pow(2, LOG_2_ALIGN)));
 	new_node->next_offset = (char*)next_node(node) - (char*)new_node;
 	new_node->free = TRUE;
