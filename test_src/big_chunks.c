dde0e4f25c10afa65f60f662ea088b725222a357
Max Gautier <mgautier@student.42.fr>
2019-05-17 15:25:42 +0200

    More tests

--- test_src/big_chunks.c
+++ test_src/big_chunks.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   several_malloc_several_free.c                      :+:      :+:    :+:   */
+/*   big_chunks.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/14 16:51:02 by                   #+#    #+#             */
-/*   Updated: 2019/05/14 16:51:02 by                  ###   ########.fr       */
+/*   Created: 2019/05/17 15:20:37 by                   #+#    #+#             */
+/*   Updated: 2019/05/17 15:20:37 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#define NB_ADDRESS 4000
+#define TEST_SIZE 4000
+#define CHUNK_SIZE 2000
 
 int	main(void)
 {
-	void	*addresses[NB_ADDRESS];
+	void	*address[TEST_SIZE];
 	size_t	index;
 
 	index = 0;
-	while (index < NB_ADDRESS)
+	while (index < TEST_SIZE)
 	{
-		addresses[index] = malloc(1);
+		address[index] = malloc(CHUNK_SIZE);
 		index++;
 	}
 	while (index != 0)
 	{
 		index--;
-		free(addresses[index]);
+		free(address[index]);
 	}
 	return (0);
 }
