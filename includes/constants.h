7df0f3717f2e5629daf4cea3979aaaf22da6784d
Max Gautier <mgautier@student.42.fr>
2019-05-20 16:18:12 +0200

    page_biggest

--- includes/constants.h
+++ includes/constants.h
@@ -56,5 +56,6 @@ struct s_alloc_zone const *get_page_from_first_node(struct s_free_node const *);
 size_t	size_category_to_size(size_t size_category);
 size_t	size_to_size_category(size_t size_category);
 size_t	node_size_category(struct s_free_node const *node);
+size_t	page_biggest_free_node_category(struct s_alloc_zone const *page);
 
 #endif
--- size_category.c
+++ size_category.c
@@ -12,6 +12,7 @@
 
 #include <stddef.h>
 #include "constants.h"
+#include "max.h"
 
 size_t	size_category_to_size(size_t size_category)
 {
@@ -35,4 +36,21 @@ size_t	size_to_size_category(size_t size)
 size_t	node_size_category(struct s_free_node const *node)
 {
 	return (size_to_size_category(node_size(node)));
+}
+
+size_t	page_biggest_free_node_category(struct s_alloc_zone const *page)
+{
+	size_t						category;
+	struct s_free_node const	*node;
+
+	category = 0;
+	node = get_first_node(page);
+	while (1)
+	{
+		category = size_t_max(category, node_size_category(node));
+		if (is_last_node(node))
+			break ;
+		node = next_node(node);
+	}
+	return (category);
 }
