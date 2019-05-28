b95aaa85943a6ee96a987522cb1a4a2b7dc08749
Max Gautier <mgautier@student.42.fr>
2019-05-28 16:12:19 +0200

    norme on nephews cases

--- rbtree/rbtree_delete_nephews_case.c
+++ rbtree/rbtree_delete_nephews_case.c
@@ -13,24 +13,30 @@
 #include "rb_tree.h"
 #include "rb_tree_delete_nephew_cases.h"
 
-void	parent_sibling_nephews_black(
+static struct s_rbtree	*grand_child(struct s_rbtree *tree,
+		int const child_side, int const grand_child_side)
+{
+	return (tree->children[child_side]->children[grand_child_side]);
+}
+
+void					parent_sibling_nephews_black(
 		struct s_rbtree **tree,
 		int side)
 {
 	(*tree)->children[!side]->color = RED;
 }
 
-void	parent_red_sib_neph_black(struct s_rbtree **tree, int side)
+void					parent_red_sib_neph_black(
+		struct s_rbtree **tree, int side)
 {
 	(*tree)->color = BLACK;
 	(*tree)->children[!side]->color = RED;
 }
 
-void	outer_red_nephew(struct s_rbtree **tree, int side)
+void					outer_red_nephew(struct s_rbtree **tree, int side)
 {
 	enum e_color const		tmp = (*tree)->color;
-	struct s_rbtree *const	outer_nephew = (*tree)
-		->children[!side]->children[!side];
+	struct s_rbtree *const	outer_nephew = grand_child(*tree, !side, !side);
 
 	(*tree)->color = BLACK;
 	(side ? right_rotate : left_rotate)(tree);
@@ -38,10 +44,10 @@ void	outer_red_nephew(struct s_rbtree **tree, int side)
 	outer_nephew->color = BLACK;
 }
 
-void	inner_red_nephew(struct s_rbtree **tree, int side)
+void					inner_red_nephew(struct s_rbtree **tree, int side)
 {
 	struct s_rbtree **const	sibling = &(*tree)->children[!side];
-	struct s_rbtree *const		inner_nephew = (*sibling)->children[side];
+	struct s_rbtree *const	inner_nephew = (*sibling)->children[side];
 
 	(side ? left_rotate : right_rotate)(sibling);
 	(*sibling)->color = RED;
