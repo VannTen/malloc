b1df00a825c456da3db21c8b57ef729f904bb9cc
Max Gautier <mgautier@student.42.fr>
2019-05-31 12:34:35 +0200

    Change data structs

--- includes/malloc_structures.h
+++ includes/malloc_structures.h
@@ -12,14 +12,22 @@
 
 #ifndef MALLOC_STRUCTURES_H
 # define MALLOC_STRUCTURES_H
-# include "rb_tree.h"
-# include "constants.h"
+
+/*
+** The doubly linked lists shall hold page which have at least one free node
+** (for their category)
+** The lists shall be sorted in ascending order by total free space available,
+** in order to have the most filled up pages at the top.
+**
+** Once they are totally filled, they should be removed from the list and stored
+** elsewhere.
+*/
 
 struct	s_alloc_categorie
 {
-	struct s_rbtree		*page_tree;
-	struct s_alloc_zone	*block_by_size[SMALL_MAX];
-	struct s_list		*partially_used_pages[2];
+	struct s_rbtree			*page_tree;
+	struct s_double_list	*tinies;
+	struct s_double_list	*smalls;
 };
 
 extern struct s_alloc_categorie g_alloc_zones;
