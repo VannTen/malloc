43d87a875f436585a25a78e4c0e96fa81950e37b
Max Gautier <mgautier@student.42.fr>
2019-06-07 15:59:49 +0200

    Print page address

--- alloc_zone_print.c
+++ alloc_zone_print.c
@@ -23,13 +23,13 @@ static char const	*select_type_string(struct s_alloc_zone const *zone)
 	char const					*type_string;
 
 	if (is_large(zone))
-		type_string = "LARGE\n";
+		type_string = "LARGE";
 	else if (is_tiny(zone))
-		type_string = "TINY\n";
+		type_string = "TINY";
 	else if (is_small(zone))
-		type_string = "SMALL\n";
+		type_string = "SMALL";
 	else
-		type_string = "ERROR\n";
+		type_string = "ERROR";
 	return (type_string);
 }
 
@@ -62,9 +62,19 @@ static size_t		write_node(struct s_free_node const *node, char *const string)
 	return (string_index + sizeof(" octets\n"));
 }
 
+static void			print_page_infos(struct s_alloc_zone const *const zone)
+{
+	char	buf[100];
+
+	ft_strcpy(buf, select_type_string(zone));
+	ft_strcpy(buf + ft_strlen(buf), " : ");
+	buf[ft_strlen(buf) + write_pointer(zone, buf + ft_strlen(buf))] = '\0';
+	ft_strcpy(buf + ft_strlen(buf), "\n");
+	write(STDOUT_FILENO, buf, ft_strlen(buf));
+}
+
 size_t				alloc_zone_print(struct s_alloc_zone const *zone)
 {
-	char const *const			type_string = select_type_string(zone);
 	size_t						size;
 	size_t						size_allocated;
 	struct s_free_node const	*node;
@@ -73,7 +83,7 @@ size_t				alloc_zone_print(struct s_alloc_zone const *zone)
 	assert(zone != NULL);
 	node = get_first_node(zone);
 	size_allocated = 0;
-	write(STDOUT_FILENO, type_string, ft_strlen(type_string));
+	print_page_infos(zone);
 	while (1)
 	{
 		if (!node->free)
