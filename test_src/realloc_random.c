6900b7d16a862d5f9937baf175bbc4e558da021e
Max Gautier <mgautier@student.42.fr>
2019-06-07 14:45:11 +0200

    realloc tests with random

---
+++ test_src/realloc_random.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   realloc_random.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  <>                                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/07 14:25:54 by                   #+#    #+#             */
+/*   Updated: 2019/06/07 14:25:54 by                  ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "memutils.h"
+#define SIZE_TEST 50000
+#define MAX_ALLOC_SIZE 10000
+#define FILL_BYTE '$'
+
+static void	alloc_ints(void **address)
+{
+	size_t	index;
+	size_t	alloc_size;
+
+	index = 0;
+	while (index < SIZE_TEST)
+	{
+		alloc_size = rand() % MAX_ALLOC_SIZE;
+		address[index] = malloc(alloc_size);
+		ft_memset(address[index], FILL_BYTE, alloc_size);
+		index++;
+	}
+}
+
+int	main(void)
+{
+	void	*address[SIZE_TEST];
+	size_t	index;
+	size_t	alloc_size;
+
+	alloc_ints(address);
+	index = 1;
+	while (index < SIZE_TEST)
+	{
+		free(address[index]);
+		address[index] = NULL;
+		index += 2;
+	}
+	index = 0;
+	while (index < SIZE_TEST)
+	{
+		alloc_size = rand() % MAX_ALLOC_SIZE;
+		address[index] = realloc(address[index], alloc_size);
+		ft_memset(address[index], FILL_BYTE, alloc_size);
+		index++;
+		index += 2;
+	}
+	return (0);
+}
