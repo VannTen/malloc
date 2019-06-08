34f392f4dcbac63201bbdf7d6f5b51c925fa336e
Max Gautier <mgautier@student.42.fr>
2019-06-08 17:17:19 +0200

    Also tests small

--- test_src/alloc_zone_get_space.c
+++ test_src/alloc_zone_get_space.c
@@ -13,7 +13,7 @@
 #include "alloc_zone.h"
 #include "constants.h"
 
-int	main(void)
+static int	test_tiny(void)
 {
 	struct s_alloc_zone	*const	zone = create_zone(tiny_page_size());
 	size_t const				expected_initial_size =
@@ -21,9 +21,7 @@ int	main(void)
 		- offset_zone_start_first_address();
 	size_t						node_taken;
 	size_t const				node_to_take = 10;
-	size_t						size;
 
-	size = alloc_zone_free_space(zone);
 	if (alloc_zone_free_space(zone) != expected_initial_size)
 		return (1);
 	node_taken = 0;
@@ -32,7 +30,34 @@ int	main(void)
 		get_first_fit(zone, tiny_size_limit());
 		node_taken++;
 	}
-	return (!(alloc_zone_free_space(zone)
+	return (alloc_zone_free_space(zone)
 			== expected_initial_size
-			- node_taken * (tiny_size_limit() + sizeof(struct s_free_node))));
+			- node_taken * (tiny_size_limit() + sizeof(struct s_free_node)));
+}
+
+static int	test_small(void)
+{
+	struct s_alloc_zone	*const	zone = create_zone(small_page_size());
+	size_t const				expected_initial_size =
+		small_page_size()
+		- offset_zone_start_first_address();
+	size_t						node_taken;
+	size_t const				node_to_take = 10;
+
+	if (alloc_zone_free_space(zone) != expected_initial_size)
+		return (1);
+	node_taken = 0;
+	while (node_taken < node_to_take)
+	{
+		get_first_fit(zone, small_size_limit());
+		node_taken++;
+	}
+	return (alloc_zone_free_space(zone)
+			== expected_initial_size
+			- node_taken * (small_size_limit() + sizeof(struct s_free_node)));
+}
+
+int			main(void)
+{
+	return (!(test_tiny() && test_small()));
 }
