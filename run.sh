c076c8f74ebce2358cd1be94ba2c14b7eaba8954
Max Gautier <mgautier@student.42.fr>
2019-06-12 20:21:05 +0200

    Variales for testing lib on Linux

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export LD_LIBRAY_PATH=.
+export LD_PRELOAD=$PWD/libft_malloc.so
 export DYLD_LIBRARY_PATH=.
 export DYLD_INSERT_LIBRARIES="libft_malloc.so"
 export DYLD_FORCE_FLAT_NAMESPACE=1
